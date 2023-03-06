a5885e2043612e670a227e37ac86ecba0c2e9128
BryanMuir-hardinge <114984969+BryanMuir1@users.noreply.github.com>
2023-03-06 21:48:27 +0000

    commit

--- 2DPlatformer-packed/Assets/Scripts/BetterPlatformer/BetterCharacterController.cs
+++ 2DPlatformer-packed/Assets/Scripts/BetterPlatformer/BetterCharacterController.cs
@@ -12,16 +12,32 @@ using UnityEngine.Events;
 public class BetterCharacterController : MonoBehaviour
 {
     protected bool facingRight = true;
+
+    //Jump Values
     protected bool jumped;
     public int maxJumps;
     protected int currentjumpCount;
-
-    public float speed = 5.0f;
     public float jumpForce = 1000;
-
-    private float horizInput;
-
     public bool grounded;
+
+    //Run value
+    private float horizInput;
+    public float speed = 5.0f;
+    public float shiftSpeed = 10.0f;
+    public float resetSpeed = 5.0f;
+
+    //For walls
+    public bool isWallSliding = false;
+    public Transform wallCheck;
+    public LayerMask wallLayer;
+    private float wallSlidingSpeed = 2f;
+
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    public Vector2 wallJumpingPower = new Vector2(8f, 16f);
 
     public Rigidbody2D rb;
 
@@ -49,8 +65,12 @@ public class BetterCharacterController : MonoBehaviour
         Vector2 boxCenter = new Vector2(transform.position.x + charCollision.offset.x, transform.position.y + -(playerSize.y + boxSize.y - 0.01f) + charCollision.offset.y);
         grounded = Physics2D.OverlapBox(boxCenter, boxSize, 0f, groundedLayers) != null;
 
-        //Move Character
-        rb.velocity = new Vector2(horizInput * speed * Time.fixedDeltaTime, rb.velocity.y);
+        if(!isWallJumping)
+        {
+            //Move Character
+            rb.velocity = new Vector2(horizInput * speed * Time.fixedDeltaTime, rb.velocity.y);
+        }
+
 
         //Jump
         if (jumped == true)
@@ -62,7 +82,8 @@ public class BetterCharacterController : MonoBehaviour
 
             jumped = false;
         }
-
+        if (!isWallJumping)
+        {
         // Detect if character sprite needs flipping.
         if (horizInput > 0 && !facingRight)
         {
@@ -72,6 +93,8 @@ public class BetterCharacterController : MonoBehaviour
         {
             FlipSprite();
         }
+        }
+
     }
 
     void Update()
@@ -97,7 +120,19 @@ public class BetterCharacterController : MonoBehaviour
         }
 
         //Get Player input 
-        horizInput = Input.GetAxis("Horizontal");     
+        horizInput = Input.GetAxis("Horizontal");
+
+        //Run
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = shiftSpeed;
+            Debug.Log("Should run");
+
+            StartCoroutine (ResetSpeed());
+        }
+
+        wallSlide();
+        wallJump();
     }
 
     public void Onlanding()
@@ -109,5 +144,64 @@ public class BetterCharacterController : MonoBehaviour
     {
         facingRight = !facingRight;
         transform.Rotate(0, 180, 0);
+    }
+    
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer) ;
+    }
+    private void wallSlide()
+    {
+        if(IsWalled() && !grounded)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+    private void wallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if (transform.localScale.x != wallJumpingDirection)
+            {
+                facingRight = !facingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+    
+    IEnumerator ResetSpeed()
+    {
+        yield return new WaitForSeconds(2.0f);
+        speed = resetSpeed;
+        Debug.Log("reset");
     }
 }
