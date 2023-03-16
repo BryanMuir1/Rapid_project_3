f3404d5d9b443a39651b840ed7aef21b14d1eae4
BryanMuir-hardinge <114984969+BryanMuir1@users.noreply.github.com>
2023-03-16 14:46:11 +0000

    commit fixed enemy

--- 2DPlatformer-packed/Assets/Scripts/EN_Frog.cs
+++ 2DPlatformer-packed/Assets/Scripts/EN_Frog.cs
@@ -20,6 +20,12 @@ public class EN_Frog : MonoBehaviour
     protected CapsuleCollider2D characterCollider;
     
     public float speed = 5.0f;
+
+    bool mustTurn;
+    public Transform floorCheck;
+    public LayerMask groundLayer;
+    bool Patrol; 
+
     void Awake()
     {
 
@@ -29,37 +35,70 @@ public class EN_Frog : MonoBehaviour
         floorTrigger = GetComponent<BoxCollider2D>();
 
         characterCollider = GetComponent<CapsuleCollider2D>() ;
+        Patrol = true; 
+        //if(facingRight == false)
+        //{
+        //    Flip();
+        //}
 
-        if(facingRight == false)
+        
+    }
+    private void Update()
+    {
+        Debug.Log(mustTurn);
+        //if (Patrol)
+        //{
+            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+        //}
+
+        if (mustTurn == true)
         {
-            Flip();
+            Flip(); 
         }
+       
     }
     void FixedUpdate()
     {
-        if(Mathf.Abs(horizonInput * (rb.velocity.x)) < maxSpeed)
-        {
-            rb.AddForce(horizonInput * Vector2.right * maxForce);
+        mustTurn = !Physics2D.OverlapCircle(floorCheck.position, 0.5f, groundLayer);
 
-        }
-        if (Mathf.Abs(rb.velocity.x) > maxSpeed)
-        {
-            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
-        }
+        //if(Mathf.Abs(horizonInput * (rb.velocity.x)) < maxSpeed)
+        //{
+        //    rb.AddForce(horizonInput * Vector2.right * maxForce);
+
+        //}
+        //if (Mathf.Abs(rb.velocity.x) > maxSpeed)
+        //{
+        //    rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+        //}
     }
     void Flip()
     {
-        facingRight = !facingRight;
-        transform.Rotate(0, 180, 0);
+        Patrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        maxSpeed *= -1;  
+        Patrol = true;
     }
-    void OneTriggerExit2D(Collider2D other)
+
+    void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Level")
+        //if (other.gameObject.layer == LayerMask.NameToLayer("Level"))
+        //{
+        //    Flip();        
+        //}
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.tag == "Player")
         {
-            if(other.attachedRigidbody.velocity.y < -0.1f)
+            if (other.rigidbody.velocity.y < -0.1f)
             {
                 horizonInput = 0;
-                Destroy(other.gameObject);
+                Destroy(gameObject, 0.5f);
+            }
+            else
+            {
+                //Destroy(other.gameObject);
             }
         }
     }
